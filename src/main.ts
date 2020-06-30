2695ca36b321fd7dafa71c7d6c7c70cc6deaeae8
Antoine <karagan.app@gmail.com>
2020-06-30 14:10:35 +0200

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   const logger: Logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'static'));
-  await app.listen(3000, () => {
-    logger.log('Nest app listening on port 3000');
+  await app.listen(3001, () => {
+    logger.log('Nest app listening on port 3001');
   });
 }
 bootstrap();
